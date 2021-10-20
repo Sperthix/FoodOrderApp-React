f3faf2c7573c1ece84cea06faa4eac11fd4c9803
Sperthix <matusbuci@gmail.com>
2021-10-20 14:29:13 +0200

    fixed problem with not listing items in cart

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -8,12 +8,15 @@ import CartContext from "../../store/cart-context";
 const Cart = (props) => {
   const cartCtx = useContext(CartContext);
 
+  const totalPrice = `${cartCtx.totalPrice.toFixed(2)} €`;
+  const cartIsNotEmpty = cartCtx.totalAmount > 0;
+
   const cartItemRemoveHandler = (id) => {};
   const cartItemAddHandler = (item) => {};
 
   const cartItems = (
-    <ul className={styles}>
-      {cartCtx.items.map((item) => {
+    <ul className={styles['cart-items']}>
+      {cartCtx.items.map((item) => (
         <CartItem
           key={item.id}
           name={item.name}
@@ -21,13 +24,10 @@ const Cart = (props) => {
           price={item.price}
           onRemove={cartItemRemoveHandler.bind(null, item.id)}
           onAdd={cartItemAddHandler.bind(null, item)}
-        />;
-      })}
+        />
+      ))}
     </ul>
-  );
-
-  const totalPrice = `${cartCtx.totalPrice.toFixed(2)} €`;
-  const cartIsNotEmpty = cartCtx.totalAmount > 0;
+  )
 
   return (
     <Modal onClose={props.onClose}>
