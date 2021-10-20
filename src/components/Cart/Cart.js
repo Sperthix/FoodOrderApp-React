4864fa8c462a32a5e7e223a3c9f5945eee2328ba
Sperthix <matusbuci@gmail.com>
2021-10-20 14:12:47 +0200

    outputing cart with context data

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,25 +1,46 @@
-import Modal from "../UI/Modal";
+import { useContext } from "react";
+
 import styles from "./Cart.module.css";
+import Modal from "../UI/Modal";
+import CartItem from "./CartItem";
+import CartContext from "../../store/cart-context";
 
 const Cart = (props) => {
+  const cartCtx = useContext(CartContext);
+
+  const cartItemRemoveHandler = (id) => {};
+  const cartItemAddHandler = (item) => {};
+
   const cartItems = (
     <ul className={styles}>
-      {[{ id: "c1", name: "sushi" }].map((item) => {
-        <li>{item.name}</li>;
+      {cartCtx.items.map((item) => {
+        <CartItem
+          key={item.id}
+          name={item.name}
+          amount={item.amount}
+          price={item.price}
+          onRemove={cartItemRemoveHandler.bind(null, item.id)}
+          onAdd={cartItemAddHandler.bind(null, item)}
+        />;
       })}
     </ul>
   );
+
+  const totalPrice = `${cartCtx.totalPrice.toFixed(2)} €`;
+  const cartIsNotEmpty = cartCtx.totalAmount > 0;
 
   return (
     <Modal onClose={props.onClose}>
       {cartItems}
       <div className={styles.total}>
         <span>Celkom</span>
-        <span>69.69</span>
+        <span>{totalPrice}</span>
       </div>
       <div className={styles.actions}>
-          <button className={styles['button--alt']} onClick={props.onClose}>Zavri</button>
-          <button className={styles.button}>Objednaj</button>
+        <button className={styles["button--alt"]} onClick={props.onClose}>
+          Zavri
+        </button>
+        {cartIsNotEmpty && <button className={styles.button}>Objednaj</button>}
       </div>
     </Modal>
   );
