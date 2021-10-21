9faaf79edc35f8ccc242105b86901f6195a64ff6
Sperthix <matusbuci@gmail.com>
2021-10-21 16:15:49 +0200

    added cart change animation

--- src/components/Layout/HeaderButton.js
+++ src/components/Layout/HeaderButton.js
@@ -1,14 +1,32 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 
 import styles from "./HeaderButton.module.css";
 import CartIcon from "../Cart/CartIcon";
 import CartContext from "../../store/cart-context";
 
 const HeaderButton = (props) => {
-    const cartCtx = useContext(CartContext);
+  const [btnBump, setBtnBump] = useState(false);
+  const cartCtx = useContext(CartContext);
+
+  const { items } = cartCtx;
+  const btnStyles = `${styles.button} ${btnBump ? styles.bump : ""}`;
+
+  useEffect(() => {
+    if (items.length === 0) {
+      return;
+    } else {
+      setBtnBump(true);
+      const timer = setTimeout(() => {
+        setBtnBump(false);
+      }, 300);
+      return () => {
+        clearTimeout(timer);
+      }
+    }
+  }, [items]);
 
   return (
-    <button className={styles.button} onClick={props.onClick}>
+    <button className={btnStyles} onClick={props.onClick}>
       <span className={styles.icon}>
         <CartIcon />
       </span>
